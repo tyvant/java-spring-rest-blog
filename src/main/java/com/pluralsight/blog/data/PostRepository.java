dcefb4f23ea80f875f9a7be1be9c5fdcc0f74d0a
tyvant <tyvant@gmail.com>
2022-02-27 20:45:03 +0100

    [PLURALSIGHT] Create the Post Repository and Save data.

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -1,29 +1,20 @@
 package com.pluralsight.blog.data;
 
-import com.pluralsight.blog.model.Author;
 import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-@Component
-public class PostRepository {
-    public List<Post> findAll() {
-        return new ArrayList<>();
-    }
 
-    public List<Post> saveAll(List<Post> posts) {
-        return new ArrayList<>();
-    }
-
-    public Post save(Post post) {
-        return null;
-    }
-
-    public Optional<Post> findById(Long id) {
-        return null;
+@Repository
+public interface PostRepository extends JpaRepository<Post,Long> {
+     @RestResource(rel = "contains-title",path="containsTitle")
+    default List<Post> findByTitleContaining(final String title) {
+        return findAll().stream().filter(c -> c.getTitle().contains(title))
+                .collect(Collectors.toList());
     }
 }
